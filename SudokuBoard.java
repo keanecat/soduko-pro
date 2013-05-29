f2032dfd0fe935897e826d07d7c381304f4ad5bc
clickhere9 <polaris92+github@gmail.com>
2013-05-29 17:59:01 +1000

    Add validity tests to SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -13,6 +13,12 @@ public class SudokuBoard {
         board = new int[size][size];
         this.size = size;
         this.box_size = (int) Math.sqrt(size);
+        
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+        		board[i][j]=0;
+        	}
+        }
     }
     
     /**
@@ -23,6 +29,22 @@ public class SudokuBoard {
         this(board.length);
         this.board = board;
     }
+    
+    private void copyBoard(int[][] from){
+    	for(int i=0;i<size;i++){
+    		for(int j=0;j<size;j++){
+    			this.board[i][j] = from[i][j];
+    		}
+    	}
+    }
+    
+    public SudokuBoard(SudokuBoard board) {
+        this.size = board.size;
+        this.box_size = board.box_size;
+    	this.board = new int[size][size];
+    	copyBoard(board.board);
+    }
+    
     
     /**
      * Puts a number into a specific cell.
@@ -43,4 +65,122 @@ public class SudokuBoard {
     public int getCell(int row, int col) {
         return board[row][col];
     }
+    
+    public boolean isValid(int num, int row, int col){
+    	
+    	int quadrant = 0;
+    	
+    	//Ascertain which quadrant row,col fall into
+    	if(row < box_size){
+    		if(col < box_size){ //3
+        		quadrant = 1;
+        	} else if (col >= box_size && col <(box_size*2)){ //3, 6 
+        		quadrant = 2;
+        	} else if (col >= (box_size*2) && col < size){ // 6,9
+        		quadrant = 3;
+        	}
+    	} else if (row >= box_size && row <(box_size*2)){
+    		if(col < box_size){
+        		quadrant = 4;
+        	} else if (col >= box_size && col <(box_size*2)){
+        		quadrant = 5;
+        	} else if (col >= (box_size*2) && col < size){
+        		quadrant = 6;
+        	}
+    	} else if (row >= (box_size*2) && row < size){
+    		if(col < box_size){
+    			quadrant = 7;
+        	} else if (col >= box_size && col <(box_size*2)){
+        		quadrant = 8;
+        	} else if (col >= (box_size*2) && col < size){
+        		quadrant = 9;
+        	}
+    	}
+    	
+    	//If it's not a valid move in the quadrant
+    	if(!validInQuadrant(quadrant,num)){
+    		return false;
+    	} 
+    	
+    	//If it's not a valid move in the row & column it's in
+    	if(!validInRowCol(num,row,col)){
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
+    private boolean validInRowCol(int num, int row, int col){
+    	for(int i=0;i<size;i++){
+    		if(board[i][col] == num){
+    			return false;
+    		}
+    		if(board[row][i] == num){
+    			return false;
+    		}
+    	}
+    	return true;
+    }
+    
+    private boolean validInQuadrant(int quad, int num){
+    	
+    	if (quad <= 3){
+    		for(int row=0;row<3;row++){
+    			for(int col=box_size*(quad-1);col<quad*box_size;col++){
+    				if(num == board[row][col]){
+    					return false;
+    				}
+    			}
+    		}
+    	} else if (quad >=4 && quad <=6){
+    		for(int row=3;row<6;row++){
+    			for(int col=box_size*(quad-4);col<(quad-3)*box_size;col++){
+    				if(num == board[row][col]){
+    					return false;
+    				}
+    			}
+    		}
+    	} else {
+    		for(int row=6;row<9;row++){
+    			for(int col=box_size*(quad-7);col<(quad-6)*box_size;col++){
+    				if(num == board[row][col]){
+    					return false;
+    				}
+    			}
+    		}
+    	}
+    	return true;
+    }
+    
+	public int[] findBlankSquare(){
+		int[] retVal = {-1,-1};
+		//For each row
+		for(int row=0;row<size;row++){
+			//And each column
+			for(int col=0;col<size;col++){
+				//If square is blank
+				if(board[row][col]==0){
+					retVal[0]=row;
+					retVal[1]=col;
+					return retVal;
+				}
+			}
+		}
+		return retVal;
+	}
+    
+	
+	public void printBoard(){
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				if(board[i][j]!= 0){
+					System.out.print(board[i][j]+" ");
+				} else {
+					System.out.println("   ");
+				}
+				
+			}
+			System.out.print("\n");
+		}
+	}
 }
